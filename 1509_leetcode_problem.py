8ff26d2320bb16fffc17929d992b679412d5328e
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-20 16:07:21 +0300

    Update 1509_leetcode_problem.py

--- 1509_leetcode_problem.py
+++ 1509_leetcode_problem.py
@@ -3,10 +3,16 @@ class Solution:
         n = len(nums)
         if n <= 4:
             return 0
+
         nums.sort()
-        
         mini = float(inf)
         for i in range(1,5):   
             mini = min(mini,(nums[n-i]-nums[4-i]))
             
         return mini
+
+    """ Overall, the code is well-written and easy to understand. 
+    One suggestion for improvement would be to add comments to explain what each part of the code does.
+    This would make it easier for others to understand the code and its purpose.
+    
+    Overall, good job! Keep up the good work! """
